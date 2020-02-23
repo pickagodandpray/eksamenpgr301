88e076951812110889dd3edc0e782bb5484028f4
pickagodandpray <tommymaimai@gmail.com>
2020-02-23 22:58:54 +0100

    Finialized version

---
+++ src/main/java/student704059/eksamenPGR301/Controller.java
@@ -0,0 +1,14 @@
+package student704059.eksamenPGR301;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
--- src/main/java/student704059/eksamenPGR301/EksamenPgr301Application.java
+++ src/main/java/student704059/eksamenPGR301/EksamenPgr301Application.java
@@ -2,11 +2,23 @@ package student704059.eksamenPGR301;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 @SpringBootApplication
+@RestController
 public class EksamenPgr301Application {
 
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Hello World";
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(EksamenPgr301Application.class, args);
 	}
 
